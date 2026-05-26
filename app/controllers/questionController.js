89b5a772731df9c6f9858e3de7e767d2be511560
Rivaldo Caga <rivaldocaga6@gmail.com>
2026-05-26 17:55:04 +0800

    Added Search Questions based on Query path

--- app/controllers/questionController.js
+++ app/controllers/questionController.js
@@ -73,10 +73,70 @@ const getQuestions = async (req, res) => {
   }
 };
 
-// Add Questions
+// Search questions based on name
+const searchQuestion = async (req, res) => {
+  const { name, difficulty, topic, subject } = req.query;
 
-// Add Subjects
+  try {
+    const db = req.app.locals.db;
 
-// Add Topics
+    let query = {};
 
-module.exports = { getQuestions };
+    // Validate input depending on the search query
+    if (name) {
+      query.text = { $regex: name, $options: "i" };
+    }
+
+    if (difficulty) {
+      const difficultyArray = difficulty.split(",");
+
+      query.difficulty = { $in: difficultyArray };
+    }
+
+    // Check if topic exist in the database
+    if (topic) {
+      const topicDb = await db.collection("topics").findOne({ code: topic });
+
+      if (!topicDb)
+        return res.status(400).json({ message: "Topic does not exist" });
+
+      query.topic = topicDb._id;
+    }
+
+    // Check if subject exist in the database
+    if (subject) {
+      const subjectDb = await db
+        .collection("subjects")
+        .findOne({ code: subject });
+
+      if (!subjectDb)
+        return res.status(400).json({ message: "Topic does not exist" });
+
+      query.subject = subjectDb._id;
+    }
+
+    // Get results based on the query
+    const result = await db.collection("questions").find(query).toArray();
+
+    // Check if question is found
+    if (result.length == 0) {
+      return res.status(404).json({ message: "Question not found" });
+    }
+
+    return res.status(200).json({ message: "Question found", result: result });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+// Filter questions based on query (topic, subject, difficulty)
+
+// Add question (only teacher)
+
+// Edit question (only teacher that has the question)
+
+// Remove question (only for teacher that has the question)
+
+module.exports = { getQuestions, searchQuestion };
--- app/routes/questionRoutes.js
+++ app/routes/questionRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const route = express.Router();
-const { getQuestions } = require("../controllers/questionController");
+const {
+  getQuestions,
+  searchQuestion,
+} = require("../controllers/questionController");
 
 route.route("/").get(getQuestions);
+route.route("/search").get(searchQuestion);
 
 module.exports = route;
